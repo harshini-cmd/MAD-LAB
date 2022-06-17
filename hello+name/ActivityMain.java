562e70c5968452d73911fc61b263a6dc9c1a1c9c
19WH1A1233 <65216515+harshini-cmd@users.noreply.github.com>
2022-06-17 22:26:57 +0530

    Create ActivityMain.java

---
+++ hello+name/ActivityMain.java
@@ -0,0 +1,29 @@
+package com.example.helloname;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+ EditText name;
+ Button Add_button;
+ TextView tv;
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_main);
+ name = (EditText) findViewById(R.id.editText);
+ Add_button = (Button) findViewById(R.id.add_button);
+ tv = (TextView) findViewById(R.id.textView_answer);
+ Add_button.setOnClickListener(new View.OnClickListener(){
+ public void onClick(View view){
+Mobile Application Development Lab 19WH1A1257
+ Date:-
+Department of Information Technology Page | 3 
+ BVRIT HYDERABAD College of Engineering for Women
+ String Name = name.getText().toString();
+ tv.setText("Hello "+Name);
+ }
+ });
+ }
